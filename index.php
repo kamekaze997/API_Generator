23e815532d3970355e895b4cb2d2256ca6b38119
Ian Pimentel <ian.pimentel002@gmail.com>
2017-03-15 01:02:01 +0000

    php pratice

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+        
+             <head>
+                 
+             </head>  
+                 <body>
+                    <?php for ($i=0; $i<4; $i++){
+                     echo "<p>Hellowworlddddddd!!!</p> \n";
+                    }
+                     ?>
+                     
+                     
+                     
+             </body>
+    </html>
